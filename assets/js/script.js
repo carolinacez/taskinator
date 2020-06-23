c84b128f588a8562420c289d450569f2a0c389f2
carolina <carolinaccez@gmail.com>
2020-06-22 20:19:50 -0400

    submission form updated

--- assets/js/script.js
+++ assets/js/script.js
@@ -7,10 +7,27 @@ var tasksToDoEl = document.querySelector("#tasks-to-do");
     event.preventDefault();
 
     //console.log(event);
-
+  var taskNameInput = document.querySelector("input[name='task-name']").value;
+  //console.dir(taskNameInput); 
+  
+  var taskTypeInput = document.querySelector("select[name='task-type']").value;
+  
+  //create list item
   var listItemEl = document.createElement("li");
   listItemEl.className = "task-item";
-  listItemEl.textContent = "This is a new task.";
-  tasksToDoEl.appendChild(listItemEl);
+
+  //create div to hold task info add to list item 
+  var taskInfoEl = document.createElement("div");
+
+  // give it a class name
+  taskInfoEl.className = "task-info";
+ // add HTML content to div
+ taskInfoEl.innerHTML = "<h3 class='task-name'>" + taskNameInput + "</h3><span class='task-type'>" + taskTypeInput + "</span>";
+ 
+ listItemEl.appendChild(taskInfoEl);
+//listItemEl.textContent = taskNameInput;
+
+//add entire list item to list 
+tasksToDoEl.appendChild(listItemEl);
 }
 formEl.addEventListener("submit", createTaskHandler);
