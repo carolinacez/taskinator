cecfa761403d4925a3627e03f72ee856b8e3d343
carolina <carolinaccez@gmail.com>
2020-06-24 23:54:15 -0400

    drag event

--- assets/js/script.js
+++ assets/js/script.js
@@ -61,6 +61,8 @@ var createTaskEl = function (taskDataObj) {
     // add task id as a custom attribute
     listItemEl.setAttribute("data-task-id", taskIdCounter);
 
+    listItemEl.setAttribute("draggable", "true");
+
     //create div to hold task info add to list item 
     var taskInfoEl = document.createElement("div");
 
@@ -171,5 +173,12 @@ var taskStatusChangeHandler = function (event) {
         tasksCompletedEl.appendChild(taskSelected);
     }
 };
+var dragTaskHandler = function (event) {
+    var taskId = event.target.getAttribute("data-task-id");
+    event.dataTransfer.setData("text/plain", taskId);
+    var getId = event.dataTransfer.getData("text/plain");
+    console.log("getId:", getId, typeof getId);
+}
 pageContentEl.addEventListener("click", taskButtonHandler);
 pageContentEl.addEventListener("change", taskStatusChangeHandler);
+pageContentEl.addEventListener("dragstart", dragTaskHandler);
