b47371decd4812d5cd9d19898f4dfd46ae953050
carolina <carolinaccez@gmail.com>
2020-06-26 17:41:47 -0400

    highlight drag

--- assets/js/script.js
+++ assets/js/script.js
@@ -183,6 +183,7 @@ var dropZoneDragHandler = function (event) {
     var taskListEl = event.target.closest(".task-list");
     if (taskListEl) {
         event.preventDefault();
+        taskListEl.setAttribute("style", "background: rgba(68, 233, 255, 0.7); border-style: dashed;");
         console.dir(taskListEl);
     }
 
@@ -203,10 +204,18 @@ var dropTaskHandler = function (event) {
         statusSelectEl.selectedIndex = 2;
         dropZoneEl.appendChild(draggableElement);
     }
+    dropZoneEl.removeAttribute("style");
     dropZoneEl.appendChild(draggableElement);
 };
+var dragLeaveHandler = function (event) {
+    var taskListEl = event.target.closest(".task-list");
+    if (taskListEl) {
+        taskListEl.removeAttribute("style");
+    }
+}
 pageContentEl.addEventListener("click", taskButtonHandler);
 pageContentEl.addEventListener("change", taskStatusChangeHandler);
 pageContentEl.addEventListener("dragstart", dragTaskHandler);
 pageContentEl.addEventListener("dragover", dropZoneDragHandler);
 pageContentEl.addEventListener("drop", dropTaskHandler);
+pageContentEl.addEventListener("dragleave", dragLeaveHandler);
